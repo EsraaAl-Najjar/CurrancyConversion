d868417954c3fdfbedfc8418135a76d25791159c
Your Name <you@example.com>
2022-04-18 03:27:20 +0200

    Initial commit

---
+++ app/src/main/java/com/softic/esraa/elnajjar/ApplicationClass.kt
@@ -0,0 +1,10 @@
+package com.softic.esraa.elnajjar
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Esraa Al-Najjar
+ */
+@HiltAndroidApp
+open class ApplicationClass : Application()
---
+++ app/src/main/java/com/softic/esraa/elnajjar/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.softic.currenceyconversionappesraaalnajjar.utils
+
+/**
+ * Created by Esraa Al-Najjar
+ */
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://data.fixer.io/api/"
+        const val ACCESS_KEY= "3ceeb2e38bd289e7fa0b89f98900e740"
+    }
+
+}
